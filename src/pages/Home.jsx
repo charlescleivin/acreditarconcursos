a89731ac65f22ea0656b5f845660261331c4d93b
Charles Cleivin <charlescleivin@gmail.com>
2022-03-12 18:47:36 -0300

    added sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,259 @@
+import React from 'react'
+import heroimage from '../assets/images/heroimage.jpg'
+import Carousel, {slidesToShowPlugin} from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import {ReactComponent as Svg1} from '../assets/images/dots-svgrepo-com.svg'
+import {ReactComponent as Svg2} from '../assets/images/double-left-arrowheads-of-small-squares-lines-svgrepo-com.svg'
+
+// Sections of the Homepage (which is a landing page)
+
+export function HeroCallToAcation() {
+  return (
+    <section className=" body-font bg-gradient-to-tr from-green-400 to-green-800 p-4">
+      
+    <div className="container lg:px-5 lg:py-5 p-2 mx-auto flex justify-center flex-wrap items-center rounded-lg">
+
+      <div className="xl:w-4/6  relative lg:pr-0 pr-0 flex flex-col xl:flex-row items-center justify-center xl:gap-4 gap-2 w-full ">       
+      <div className={`w-36 h-36 absolute opacity-30 hidden lg:block -bottom-5 -left-28`}>  
+              <Svg1/>       
+      </div>
+      <div className={`w-36 h-36 absolute opacity-30 hidden lg:block -top-5 -right-24`}>  
+              <Svg1/>       
+      </div>
+
+      <div className={`w-36 transform rotate-90 h-36 absolute opacity-30 hidden lg:block -top-5 -left-24`}>  
+              <Svg2/>       
+      </div>
+          <div style={{backgroundImage: `url("${heroimage}")`, backgroundPosition: 'right' , backgroundSize: "cover", backgroundRepeat: "no-repeat"}} className={`relative flex py-16 pt-32 lg:py-0 lg:h-96 w-full items-start justify-center lg:gap-2 flex-col max-lg-xl rounded-lg overflow-hidden`}>
+            <div className={`relative z-20 lg:w-1/2 w-full pl-8`}>
+                <h1 className="title-font font-medium text-3xl text-white">Slow-carb next level shoindcgoitch ethical authentic, poko scenester</h1>
+                <p className="leading-relaxed mt-4 text-gray-100">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+            </div>  
+            <div className={`bg-black z-10 opacity-10 lg:opacity-0 absolute inset-0 w-full h-full`}></div>          
+          </div>  
+
+          <div className="w-full xl:w-96 bg-gradient-to-tr from-green-600 to-green-700 p-8 flex flex-col md:ml-auto mt-10 md:mt-0 rounded-lg overflow-hidden">
+            <h2 className="text-white text-xl font-medium title-font mb-5">Se Inscreva Agora!</h2>
+            <div className="relative mb-4">
+              <label htmlFor="full-name" className="leading-7 text-sm text-green-200">Full Name</label>
+              <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-green-200">Email</label>
+              <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <button className="text-white transition-colors duration-200 ease-in-out bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-400 rounded text-lg">Inscrever</button>
+            <p className="text-xs text-green-300 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+          </div>
+      </div>
+
+    </div>
+  </section>
+  )
+}
+
+export function CallToActionA(){
+  return(
+<section className="text-gray-500 bg-green-800 body-font">
+  <div className="container flex p-5 md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+    </div>
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
+        <br className="hidden lg:inline-block"/>readymade gluten
+      </h1>
+      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export function Testimonials() {
+  return(
+    <section className="text-gray-700 bg-gradient-to-tl from-green-500 to-green-800 body-font">
+  <div className="container lg:p-5 mx-auto"> 
+  <Carousel
+   plugins={[  
+
+  'centered',
+   'infinite',
+   {
+    resolve: slidesToShowPlugin,
+    options: {
+     numberOfSlides: 1.5,
+    },
+  }
+ ]}
+   minDraggableOffset={0.1}
+   draggable={true}
+   >
+
+<div className="p-4 w-full">
+        <div className="h-full bg-white p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a className="inline-flex items-center">
+            <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+              <span className="text-gray-500 text-sm">DESIGNER</span>
+            </span>
+          </a>
+        </div>
+      </div>  
+      
+      <div className="p-4 w-full">
+        <div className="h-full bg-gray-200 p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a className="inline-flex items-center">
+            <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+              <span className="text-gray-500 text-sm">DESIGNER</span>
+            </span>
+          </a>
+        </div>
+      </div>  
+      
+      <div className="p-4 w-full">
+        <div className="h-full bg-white p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a className="inline-flex items-center">
+            <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+              <span className="text-gray-500 text-sm">DESIGNER</span>
+            </span>
+          </a>
+        </div>
+      </div>  
+      
+      <div className="p-4 w-full">
+        <div className="h-full bg-gray-200 p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a className="inline-flex items-center">
+            <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+              <span className="text-gray-500 text-sm">DESIGNER</span>
+            </span>
+          </a>
+        </div>
+      </div>  
+
+  </Carousel>
+  </div>
+</section>
+  )
+}
+
+export function Featured() {
+  return( 
+<section className="text-gray-500 bg-gradient-to-tl bg-white body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="text-center mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">Raw Denim Heirloom Man Braid</h1>
+      <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+      <div className="flex mt-6 justify-center">
+        <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+      <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-white text-lg title-font font-medium mb-3">Shooting Stars</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <circle cx="6" cy="6" r="3"></circle>
+            <circle cx="6" cy="18" r="3"></circle>
+            <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-white text-lg title-font font-medium mb-3">The Catalyzer</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-white text-lg title-font font-medium mb-3">Neptune</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+  </div>
+</section>
+  )
+}
+
+export function ListOfAvailableClasses(){
+  return(
+    <section className={`flex items-center justify-center`}>
+      <div className={`container w-full h-96 bg-red-500`}>
+
+      </div>
+    </section>
+  )
+}
+
+
+export default function Home() {
+  return (
+    <div>
+      <HeroCallToAcation/>
+      <ListOfAvailableClasses/>
+      <Testimonials/>
+      <Featured />      
+      <CallToActionA/>
+ 
+    </div>
+  )
+}
